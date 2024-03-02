14d470cc88b2dcf9e88207a441b9c77b592d249d
dimaswahyu456 <dimaswahyu456@gmail.com>
2024-03-02 21:22:56 +0700

    rubah footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,12 +3,12 @@
         <center>
             <footer>
                 <div>
-                    <small style="font-family: 'Jetbrains_Mono'; color: white;">Copyright &copy; 2021 Rizal Ihwan</small>
+                    <small style="font-family: 'Jetbrains_Mono'; color: white;">Copyright &copy; 2024 SobatWeb</small>
                 </div>
                 <div>
-                    <a href="https://web.facebook.com/izal.whanz/"><img src="{{ asset('logo/fb.svg') }}" width="50" alt="" srcset="" ></a>
-                    <a href="https://www.instagram.com/rizalihwan94_/"><img src="{{ asset('logo/ig.svg') }}" width="50" alt="" srcset=""></a>
-                    <a href="https://wa.me/+6285770254568"><img src="{{ asset('logo/wa.svg') }}" width="50" alt="" srcset=""></a>
+                    <a href=""><img src="{{ asset('logo/fb.svg') }}" width="50" alt="" srcset=""></a>
+                    <a href="https://www.instagram.com/sobat.web/"><img src="{{ asset('logo/ig.svg') }}" width="50" alt="" srcset=""></a>
+                    <a href="https://wa.me/+6285856710103"><img src="{{ asset('logo/wa.svg') }}" width="50" alt="" srcset=""></a>
                 </div>
             </footer>
         </center>
